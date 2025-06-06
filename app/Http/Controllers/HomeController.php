04687f12a3d88db48ca7615408373e20d7d5917f
Luciano Pujol <netzcream@gmail.com>
2025-06-05 23:50:39 -0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     {
         $featured = Property::where('is_published', true)
             ->where('is_featured', true)
-            ->latest('published_at')
+            ->inRandomOrder()
             ->take(3)
             ->get();
 
